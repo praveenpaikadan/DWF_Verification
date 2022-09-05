05d8e406d714f5a3b4fe22e809804243ba61ac69
Praveen Paikadan <praveenpaikadan2@gmail.com>
2022-09-06 01:30:04 +0530

    Flow Depth and Velocity In one graph - template ready"

--- src/components/main-plot.js
+++ src/components/main-plot.js
@@ -1,44 +1,94 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import Plot from 'react-plotly.js';
 
-export const MainPlot = ({xArray, fsData, yRange}) => {
+export const MainPlot = ({xArray, fsData, yRange, label}) => {
 
-    return (
-        <div>
-            <Plot
-                // Define Data
-            var data = {[
-                {
-                x: xArray,
-                y: fsData,
-                mode:"lines",
-                name: "Observed",
-                line: {
-                    color: 'green',
-                    width: 2
-                }
-                },
-                ]}
-            
-                layout={ {
-                    xaxis: {range: [0, xArray.length], title: "Time"},
-                    yaxis: {range: [yRange[0], yRange[1]], title: "Flow(m3/s)"},
-                    title: "Flow Graph",
-                    autosize: false,
-                    width: 1080 *0.9,
-                    height: 500,
-                    margin: {
+    var trace1 = {
+        x: xArray,
+        y: fsData,
+        yaxis: 'y',
+        mode:"lines",
+        name: "Velocity",
+        line: {
+            color: 'green',
+            width: 1.5
+        }
+      };
+      
+      var trace2 = {
+        x: xArray,
+        y: fsData,
+        yaxis: 'y2',
+        mode:"lines",
+        name: "Flow",
+        line: {
+            color: 'green',
+            width: 1.5
+        }
+      };
+
+      var trace3 = {
+        x: xArray,
+        y: fsData,
+        mode:"lines",
+        name: "Depth",
+        yaxis: 'y3',
+        line: {
+            color: 'green',
+            width: 1.5
+        }
+      };
+      
+     
+
+    const [plotState, setPlotState] = useState(
+        {
+            data:  [trace1, trace2, trace3],
+            layout: {
+                xaxis: {range: [0, xArray.length], title: "Time"},
+                title: "Flow Survey",
+                autosize: false,
+                width: 1080 *0.9,
+                height: 690,
+                margin: {
                     l: 100,
                     r: 10,
                     b: 200,
                     t: 50,
                     pad: 20
-                    },
-                    paper_bgcolor: 'white',
-                    plot_bgcolor: 'white'
-            }}
+                },
+                paper_bgcolor: 'white',
+                plot_bgcolor: 'white',
+                showlegend: false,
+                yaxis: {title: "Velocity(m/s)",domain: [0, 0.31]},
+                yaxis2: {title: "Flow(m3/s)", domain: [0.33, 0.64]},
+                yaxis3: {title: "Depth(m)", domain: [0.66, 0.98]}
+            },
+
+            frames: [],
+            config: {}
+        }
+    )
+ 
+
+    return (
+        <div>
+            {/* <div>
+                <button>Flow</button>
+                <button>Depth</button>
+                <button>v</button>
+            </div> */}
+            <Plot
+                // Define Data
+                data = {plotState.data}
+                layout={plotState.layout}
+                frames={plotState.frames}
+                config={plotState.config}
+                onInitialized={(figure) => setPlotState(figure)}
+                onUpdate={(figure) => setPlotState(figure) }
             />
+            
         </div>
     )
 }
